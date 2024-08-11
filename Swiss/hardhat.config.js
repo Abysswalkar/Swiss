59ac5c133ace842ba43c61c1387e505cec1b1e69
Abysswalkar <161896448+Abysswalkar@users.noreply.github.com>
2024-08-11 18:26:36 +0000

    Third commit

--- Swiss/hardhat.config.js
+++ Swiss/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["dec5c9b2e0aad52890ac0138f26abb54120c1716f7e24a2cc13d119ade28ea83"], //Your private key starting with "0x"
+      accounts: [""], //Your private key starting with "0x"
     },
   },
 };
